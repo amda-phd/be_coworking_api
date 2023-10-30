771efc02bbc641c39c4263c249f48f49bd6d303a
amda <amda.prog@gmail.com>
2023-10-30 17:41:16 +0100

    Create room tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,30 +6,63 @@ from src.main import app
 from src.db import connect_to_mongodb
 
 @pytest.fixture
-def setup_db():
-    return connect_to_mongodb(app, True)
+def test_db():
+    return connect_to_mongodb(app, testing = True)
 
 @pytest.fixture
 def client():
     return TestClient(app)
 
-## Running this test first is a bit important, as it happens before the setup_db() fixture is called, triggering the 503 error
+## Running this test first is a bit important, as it happens before the test_db() fixture is called, triggering the 503 error
 def test_unhealthy_db(client):
     response = client.get("/health")
     assert response.status_code == 503
     assert response.json()["detail"] == "MongoDB unavailable"
 
 @pytest.mark.asyncio
-async def test_healthy_db(client, setup_db):
-    pre_docs = await setup_db["health"].count_documents({})
+async def test_healthy_db(client, test_db):
+    pre_docs = await test_db["health"].count_documents({})
     assert pre_docs == 0
     response = client.get("/health")
     assert response.status_code == 204
-    post_docs = await setup_db["health"].count_documents({})
+    post_docs = await test_db["health"].count_documents({})
     assert post_docs == pre_docs + 1
     # assert response.json() == None
 
+ok_room = {
+    "opening": "09:00",
+    "closing": "17:00",
+    "capacity": 5
+}
+id_room: None
+@pytest.mark.asyncio
+async def test_create_room_success(client, test_db):
+    response = client.post("/rooms", json = ok_room)
+    assert response.status_code == 201, response.text
+    new_room = response.json()
+    id_room = new_room["_id"]
+    db_entry = await test_db["rooms"].find_one({ "_id": id_room })
+    assert db_entry is not None
+    for key in ok_room:
+        assert new_room[key] == ok_room[key]
+        assert db_entry[key] == ok_room[key]
+    
+async def create_room_validation_error(room, client, db):
+    pre_docs = await db["rooms"].count_documents({})
+    response = client.post("/rooms", json = room)
+    assert response.status_code == 422, response.text
+    post_docs = await db["rooms"].count_documents({})
+    assert pre_docs == post_docs
 
+@pytest.mark.asyncio
+async def test_create_room_validation_errors(client, test_db):
+    rooms = [
+        { **ok_room, "capacity": -7 },
+        { **ok_room, "closing": "08:50" },
+        { **ok_room, "opening": "notwhatIwasexpecting" }
+    ]
+    for room in rooms:
+        await create_room_validation_error(room, client, test_db)
 
 # Run the tests
 if __name__ == '__main__':
