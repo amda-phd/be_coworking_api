4982a282effd97ba7d8d34cbd3a6490751d97572
amda <amda.prog@gmail.com>
2023-11-02 18:17:27 +0100

    Try to fix vercel deploy

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import uvicorn
+from decouple import config
+
+APP_PORT = config("PORT", cast=int)
+APP_HOST = config("HOST", cast=str)
 
 if __name__ == "__main__":
     uvicorn.run(
         "src.main:app",
-        reload = True
+        host = APP_HOST,
+        port = APP_PORT
     )
